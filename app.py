d0caef426ad937960f85fbfee473eb95af59a211
FarmzDev <153562375+FarmzDev@users.noreply.github.com>
2024-04-06 20:25:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from api import cdn
 import io
 
 app = Flask(__name__)
-hook = Webhook(url="https://discord.com/api/webhooks/1224099792335015936/mtKNdsa5rW49vCEBW2htgdJSeLZF2nr-Y2viblSM4zVYXfXn9Wd98GhzGzG6s9qWcNQl")
 
 @app.route("/")
 def index():
@@ -19,15 +18,3 @@ def upload_page():
 @app.route("/download")
 def download_page():
     return render_template("download.html")
-
-@app.route("/api/upload", methods=["GET", "POST"])
-def upload():
-    data = request.data
-    data = cdn.process_file_data(data)
-    return hook.send_bytes(file_bytes=base64.b64encode(data["file_bytes"]), filename=data["filename"])
-
-@app.route("/api/download", methods=["GET", "POST"])
-def download():
-    data = request.data
-    data = cdn.process_file_data(data)
-    return cdn.download_links(data["file_bytes"])
