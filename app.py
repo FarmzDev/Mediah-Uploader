de95a3549eedf3c8c2d99c88d12a13be149c3526
FarmzDev <153562375+FarmzDev@users.noreply.github.com>
2024-04-10 00:01:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import base64
 import os
 from flask import Flask, request, render_template, send_file
 import requests
-# from api import cdn
 import io
 
 app = Flask(__name__)
+
+def renew_link(url: str) -> str:
+    return requests.post("https://discord.com/api/v9/attachments/refresh-urls", json={"attachment_urls": [url]}, headers={"Authorization": config.DISCORD_TOKEN, "Content-Type": "application/json", "User-Agent": config.USER_AGENT}).json()["refreshed_urls"][0]["refreshed"]
 
 @app.route("/")
 def index():
@@ -19,19 +21,19 @@ def upload_page():
 def download_page():
     return render_template("download.html")
 
-# @app.route("/api/download/<path:url>", methods=["GET"])
-# def download(url):
-#     try:
-#         start = request.args.get("start")
-#         end = str(int(request.args.get("end"))-1)
-#         headers = {'Range': f'bytes={start}-{end}'}
-#         if start and end:
-#             response = requests.get(cdn.renew_link(url), headers=headers)
-#             if response.status_code == 206:
-#                 return response.content
-#             else:
-#                 return (f"Error: {response.status_code} - {response.reason}")
-#         else:
-#             return "Add start/end arguments!"
-#     except:
-#         return "Add start/end arguments!"
+@app.route("/api/download/<path:url>", methods=["GET"])
+def download(url):
+    try:
+        start = request.args.get("start")
+        end = str(int(request.args.get("end"))-1)
+        headers = {'Range': f'bytes={start}-{end}'}
+        if start and end:
+            response = requests.get(renew_link(url), headers=headers)
+            if response.status_code == 206:
+                return response.content
+            else:
+                return (f"Error: {response.status_code} - {response.reason}")
+        else:
+            return "Add start/end arguments!"
+    except:
+        return "Add start/end arguments!"
