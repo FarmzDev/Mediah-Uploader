ed7475180414cfa982b10e7fe7fb4966cf350aec
FarmzDev <153562375+FarmzDev@users.noreply.github.com>
2024-04-10 18:58:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def download(url):
         end = str(int(request.args.get("end"))-1)
         headers = {'Range': f'bytes={start}-{end}'}
         if start and end:
+            return renew_link(url)
             response = requests.get(renew_link(url), headers=headers)
             if response.status_code == 206:
                 return response.content
